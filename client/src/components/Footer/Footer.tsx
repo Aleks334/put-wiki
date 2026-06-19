6f858eb28f4ef5074699a6a69fab3d55fbbb6d24
Aleksander Nowicki <101459210+Aleks334@users.noreply.github.com>
2026-06-19 21:45:31 +0200

    feat: add a changes to both server and client again (#5)

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -13,7 +13,7 @@ export default function Footer() {
             </div>
             <h2>PUT - WIKI</h2>
             Nowoczesne wsparcie dla studentów Politechniki Poznańskiej. Wszystkie informacje w
-            jednym miejscu. !!!!!
+            jednym miejscu. !!!!!dsdsd
           </div>
 
           {footerSections.map((section) => (
